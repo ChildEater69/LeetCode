0cd3beffa353127a0d407222c34381633641f32c
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-06-11 13:13:11 +0530

    latest

---
+++ Daily POTD/Day18/1146. Snapshot Array Answer.cpp
@@ -0,0 +1,32 @@
+class SnapshotArray {
+public:vector<map<int,int>>v;
+int s=0;
+    SnapshotArray(int length)
+     {
+        v.resize(length);
+        for(int i=0;i<length;i++)
+        {
+            v[i][0]=0;
+        }
+    }
+    void set(int index, int val)
+     {
+        v[index][s]=val;
+    }   
+    int snap()
+     {
+        s++;
+       return s-1;
+    }   
+    int get(int index, int snap_id) 
+    {
+        if(v[index].find(snap_id)==v[index].end())
+        {
+            auto it= --v[index].lower_bound(snap_id);
+            return it->second;
+        }
+        return v[index][snap_id];
+    }
+};
+
+//Must Upvote
